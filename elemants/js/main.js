188d3ce9f7b0a8ca9ebc36b4d092edc6dd34943b
Naimur53 <naimurrhman53@gmail.com>
2021-04-11 11:22:20 +0600

    slick slider done

---
+++ elemants/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+        $('.slider-main-wrap').slick({
+            infinite: true,
+            slidesToShow: 1,
+            autoplay: true,
+            arrows: false,
+            slidesToScroll: 1
+        });
+})
