acb61e1cf0e69d66c3a5728d95ef9722fbad8bf8
Naimur53 <naimurrhman53@gmail.com>
2021-04-11 20:18:26 +0600

    done

--- elemants/js/main.js
+++ elemants/js/main.js
@@ -7,4 +7,36 @@ $(document).ready(function(){
             arrows: false,
             slidesToScroll: 1
         });
+          
+         
 })
+$(document).ready(function(){
+
+        $('.logo-slider').slick({
+            infinite: true,
+            slidesToShow: 5,
+            autoplay: true,
+            arrows: false,
+            slidesToScroll: 1
+        });
+          
+         
+})
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
